77b550cdeb45250964fee1c86be81bccdca68a42
Lalalalala <manu@korfmann.info>
2018-04-13 12:30:02 +0200

    hacky fix for loading lib

--- config/initializers/load_lib.rb
+++ config/initializers/load_lib.rb
@@ -1,2 +1,6 @@
-Rails.application.config.eager_load_paths += [Rails.root.join('lib')]
-Rails.application.config.autoload_paths += [Rails.root.join('lib')]
+# Rails.application.config.eager_load_paths += [Rails.root.join('lib')]
+# Rails.application.config.autoload_paths += [Rails.root.join('lib')]
+
+# TODO: make autoloading solution work again
+
+Dir["#{Rails.root}/lib/**/*.rb"].each { |file| require file }
