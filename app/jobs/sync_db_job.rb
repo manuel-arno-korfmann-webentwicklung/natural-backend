82ab1900407bc7cd19595dc40a36827397c1ced3
Lalalalala <manu@korfmann.info>
2018-04-24 21:52:40 +0200

    finished sync_db_job

--- app/jobs/sync_db_job.rb
+++ app/jobs/sync_db_job.rb
@@ -9,9 +9,21 @@ class SyncDbJob < ApplicationJob
       query = <<-SQL
         SELECT *
         FROM \"#{table.name}\"
+      SQL
+
+      query_extension = <<-SQL
         WHERE id NOT IN (#{table.rows.pluck(:db_id).compact.join(', ')});
       SQL
-      puts db_manager.connection.exec(query).values
+
+      query += query_extension if table.rows.any?
+
+      db_manager.connection.exec(query).each do |row|
+        natural_row = Row.create(db_id: row.values_at('id'), table: table)
+        table.columns.each do |column|
+          value = row.values_at(column.name)[0]
+          RowValue.create(row: natural_row, column: column, value: value)
+        end
+      end
     end
   end
 end
