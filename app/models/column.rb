e1646753806e252b3d0aefddb270701b98ee1e54
Lalalalala <manu@korfmann.info>
2018-05-01 15:17:27 +0200

    column validations

--- app/models/column.rb
+++ app/models/column.rb
@@ -6,7 +6,19 @@ class Column < ApplicationRecord
   after_commit :trigger_column_creation, on: :create
   before_destroy :trigger_column_destruction
 
+  validate :invalid_column_names
+
  private
+
+ def invalid_column_names
+   if name.blank?
+     errors.add(:name, "Name can't be blank")
+   end
+
+   if name == 'id'
+     errors.add(:name, "Name cannot be id")
+   end
+ end
 
   def trigger_column_creation
     CreateColumnJob.perform_later(self)
